75531a9ad5385b99403c14e34342d568fb1ed76d
Rowan Krishnan <rkrishnan8594@gmail.com>
2016-10-04 11:24:46 -0400

    Init

---
+++ app/src/main/java/com/suh/app/suh/CustomArrayAdapter.java
@@ -0,0 +1,39 @@
+package com.suh.app.suh;
+
+import android.widget.ArrayAdapter;
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.LayoutInflater;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+public class CustomArrayAdapter extends ArrayAdapter<String> {
+    private final Context context;
+    private final String[] values;
+
+    public CustomArrayAdapter(Context context, String[] values) {
+        super(context, R.layout.rowlayout, values);
+        this.context = context;
+        this.values = values;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.rowlayout, parent, false);
+        TextView textView = (TextView) rowView.findViewById(R.id.label);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
+        textView.setText(values[position]);
+        String s = values[position];
+        if (s.startsWith("Windows7") || s.startsWith("iPhone")
+                || s.startsWith("Solaris")) {
+            imageView.setImageResource(R.drawable.no);
+        } else {
+            imageView.setImageResource(R.drawable.ok);
+        }
+
+        return rowView;
+    }
+}
